ae4ee838cfda9734de7f844a1eae9bcb8ecd3412
Calvin Cartano <cacartano@gmail.com>
2025-04-17 13:47:29 -0700

    add plugin react

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
