fe1f2cf708041044468ff94dc6eb97701e408d4b
Calvin Cartano <cacartano@gmail.com>
2025-04-17 10:30:18 -0700

    set the right env vars

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -5,11 +5,11 @@ config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'online_bar',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['src/entities/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
